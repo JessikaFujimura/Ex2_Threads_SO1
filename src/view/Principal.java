d86d60499dd2e426b23d0b555ac772da664ec612
Jéssika <jessikafujimura@gmail.com>
2020-09-11 19:39:20 -0300

    Imprime o id da thread

---
+++ src/view/Principal.java
@@ -0,0 +1,14 @@
+package view;
+
+import controller.ThreadId;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		for (int idThread = 1; idThread < 6; idThread ++){
+			Thread threadId = new ThreadId(idThread);
+			threadId.start();
+		}
+	}
+
+}
